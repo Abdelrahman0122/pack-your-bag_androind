65a0ffdc5902f62a304a5f8c722f31150210e837
mahmoud08sakr <mahmoud23mostafa@gmail.com>
2023-05-06 04:27:25 +0200

    splashscreen added done

---
+++ app/src/main/java/com/example/project/slapshScreen.java
@@ -0,0 +1,28 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class slapshScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slapsh_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(slapshScreen.this,MainActivity.class);
+        startActivities(new Intent[]{intent});
+          finish();
+            }
+        },3000);
+
+
+    }
+}
